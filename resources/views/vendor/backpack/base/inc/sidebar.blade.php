b1eb50ebf0caf04c5b4e6e7f18c60fc1e562988e
Jinmo Chong <jinmochong@gmail.com>
2019-02-23 16:56:13 -0500

    set up basic homepage ui

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -1,10 +1,10 @@
-@if (backpack_auth()->check())
+{{--@if (backpack_auth()->check())--}}
     <!-- Left side column. contains the sidebar -->
     <aside class="main-sidebar">
       <!-- sidebar: style can be found in sidebar.less -->
       <section class="sidebar">
         <!-- Sidebar user panel -->
-        @include('backpack::inc.sidebar_user_panel')
+{{--        @include('backpack::inc.sidebar_user_panel')--}}
 
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
@@ -15,10 +15,12 @@
 
           @include('backpack::inc.sidebar_content')
 
+
+
           <!-- ======================================= -->
           {{-- <li class="header">Other menus</li> --}}
         </ul>
       </section>
       <!-- /.sidebar -->
     </aside>
-@endif
+{{--@endif--}}
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,3 +1,13 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+{{--<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>--}}
+{{--<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>--}}
+{{--<li class="treeview">--}}
+    {{--<a href="#"><i class="fa fa-edit"></i> <span>Analyst</span> <i class="fa fa-angle-left pull-right"></i></a>--}}
+    {{--<ul class="treeview-menu">--}}
+        {{--<li><a href="{{ "/analyst/session/create"}}"><i class="fa fa-book"></i> <span>Add Session</span></a></li>--}}
+        {{--<li><a href="{{ backpack_url('analyst/session') }}"><i class="fa fa-book"></i> <span>View Session</span></a></li>--}}
+    {{--</ul>--}}
+{{--</li>--}}
+
+<li><a href='{{ '/' }}'><i class='fa fa-tag'></i> <span>Home</span></a></li>
+<li><a href='{{ '/search' }}'><i class='fa fa-tag'></i> <span>Search</span></a></li>
